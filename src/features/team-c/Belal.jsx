a56774ec10b46b742db4dfb7e1aa07af801370d9
Belal-naghy <belal@email.com>
2025-12-14 17:35:59 +0200

    test success

--- src/features/team-c/Belal.jsx
+++ src/features/team-c/Belal.jsx
@@ -5,3 +5,13 @@ export default function Belal() {
     <div>Belal</div>
   )
 }
+
+import React from 'react'
+
+export default function login(username,password) {
+    console.log("success");
+  return (
+    <div>login</div>
+  )
+}
+
