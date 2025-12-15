3b799761449cbf31788d69e3de99612458019d6f
Mohamed Elazab <midoazab886@gmail.com>
2025-12-15 14:52:26 +0200

    Your message here

--- src/team-b/badawy.jsx
+++ src/team-b/badawy.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React from 'react'
 
 export default function badawy() {
   return (
-    <div>
-      <hi>azab</hi>
-      <h1>ahdaa 4wyA</h1>
-    </div>
-  );
+    
+    <div>badawy</div>
+  )
 }
